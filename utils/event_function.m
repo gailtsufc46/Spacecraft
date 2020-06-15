cef1b14b5bd5241274a6b83090399bf2cb0298e8
Garrett W Ailts <ailts008@ad.umn.edu>
2020-06-15 08:39:41 -0500

    Running sim without J2 removes initial raise in periapse during deorbit.

--- utils/event_function.m
+++ utils/event_function.m
@@ -1,6 +1,6 @@
 function [value,isterminal,direction] = event_function(t,x,params) %#ok<INUSL>
 R = params.Earth.Rmean;
-if norm(x(1:3))-R<=150e3
+if norm(x(1:3))-R<=100e3
     value = 0;
 else
     value = 1;
